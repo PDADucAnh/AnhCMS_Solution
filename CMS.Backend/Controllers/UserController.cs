bebcff412c1f77c56534d4ab3f50cc0af7b296c7
PDADucAnh <anh22032005@gmail.com>
2026-05-16 10:15:55 +0700

    Thử thách 2,Quản lý người dùng (User)

--- CMS.Backend/Controllers/UserController.cs
+++ CMS.Backend/Controllers/UserController.cs
@@ -5,15 +5,44 @@
  * Mô tả: tạo dữ liệu test User Controller
  */
 
+using CMS.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Backend.Controllers
 {
     public class UserController : Controller
     {
+        // Hàm Index: Hiển thị danh sách thành viên quản trị
         public IActionResult Index()
         {
-            return View();
+            // 1. Tạo danh sách Người dùng giả (Mock Data)
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    Username = "admin_thai",
+                    FullName = "Nguyễn Cao Thái",
+                    Role = "Administrator"
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "editor_01",
+                    FullName = "Phạm Đức Anh",
+                    Role = "Editor"
+                },
+                new User
+                {
+                    Id = 3,
+                    Username = "author_minh",
+                    FullName = "Phạm Nhật Vượng",
+                    Role = "Author"
+                }
+            };
+
+            // 2. Trả về View kèm theo danh sách người dùng
+            return View(users);
         }
     }
 }
