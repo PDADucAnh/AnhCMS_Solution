b0d45479565a63c5968a45fc44376cbeb77c1bf4
PDADucAnh <anh22032005@gmail.com>
2026-06-06 02:47:39 +0700

    Hoàn thành bài tập nâng cao số 3, chỉnh sửa thêm mô tả

--- CMS.Backend/Controllers/OrderDetailController.cs
+++ CMS.Backend/Controllers/OrderDetailController.cs
@@ -1,4 +1,14 @@
-﻿using CMS.Data;
+﻿/*
+    Tên: Phạm Đức Anh
+    Mã SV: 2123110135
+    Ngày tạo: 06/06/2026
+    Mô tả:    1. Truy vấn dữ liệu LINQ OrderDetail Controller
+              2. Tạo trang admin hiển thị danh sách chi tiết đơn hàng
+              3. Thiết kế giao diện quản lý chi tiết đơn hàng (CRUD) trong OrderDetailController
+              4. Sử dụng Entity Framework để kết nối và thao tác với cơ sở dữ liệu SQL Server trong OrderDetailController
+              5. Áp dụng phân quyền truy cập cho các chức năng quản lý chi tiết đơn hàng trong OrderDetailController (chỉ Admin mới được phép xóa, Editor chỉ được phép xem và sửa) trong OrderDetailController
+ */
+using CMS.Data;
 using CMS.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
