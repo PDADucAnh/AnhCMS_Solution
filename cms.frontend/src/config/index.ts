c648e7c2ffc0496087c18ba2fe947d1b9ab9cb87
PDADucAnh <anh22032005@gmail.com>
2026-07-02 22:12:06 +0700

    feat: add central CONFIG and update apiUtils to consume CONFIG values

---
+++ cms.frontend/src/config/index.ts
@@ -0,0 +1,7 @@
+const CONFIG = {
+  API_BASE_URL: process.env.REACT_APP_API_URL || 'https://localhost:7224',
+  IMAGE_BASE_URL: process.env.REACT_APP_IMAGE_BASE_URL || 'https://localhost:7224',
+  TIMEOUT: 10000,
+};
+
+export default CONFIG;
